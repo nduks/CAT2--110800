240235e34444c19d4b4122c681359a45a4e010c0
nduks <69036022+nduks@users.noreply.github.com>
2023-11-15 19:52:38 +0300

    Update Question1-i.cpp

--- Question1-i.cpp
+++ Question1-i.cpp
@@ -1,6 +1,3 @@
-/* Admission number 110800
-Introduction to Programming*/
-
 #include <iostream>
 
 using namespace std;
@@ -8,20 +5,20 @@ using namespace std;
 int main() {
     int num1, num2, num3;
 
-    cout << "Enter three numbers to find the maximum number: " << endl;
+    cout << "Enter three numbers to find the smallest number: " << endl;
     cin >> num1 >> num2 >> num3;
 
     switch (1) {
         case 1:
-            if (num1 >= num2 && num1 >= num3) {
-                cout << "The maximum number is: " << num1 << endl;
+            if (num1 <= num2 && num1 <= num3) {
+                cout << "The smallest number is: " << num1 << endl;
             } else {
                 switch (2) {
                     case 2:
-                        if (num2 >= num1 && num2 >= num3) {
-                            cout << "The maximum number is: " << num2 << endl;
+                        if (num2 <= num1 && num2 <= num3) {
+                            cout << "The smallest number is: " << num2 << endl;
                         } else {
-                            cout << "The maximum number is: " << num3 << endl;
+                            cout << "The smallest number is: " << num3 << endl;
                         }
                         break;
                 }
